127a85f360bac5079a16ceecc0ff0c7d4c41c3fa
Andy Dang <andykhangdang@gmail.com>
2025-05-05 00:38:05 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
-from poker import PokerGame
+from backend.poker import PokerGame
 
 app = FastAPI()
 
